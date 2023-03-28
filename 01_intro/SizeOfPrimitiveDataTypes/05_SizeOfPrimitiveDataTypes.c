8d2a8f61b46496e498ee833a40db45ac379716c6
srikrishna851 <119734250+srikrishna851@users.noreply.github.com>
2023-03-28 10:19:01 +0530

    Add files via upload

---
+++ 01_intro/SizeOfPrimitiveDataTypes/05_SizeOfPrimitiveDataTypes.c
@@ -0,0 +1,35 @@
+/*********************************************************************************************
+ * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
+ * this source code, object code, or executable code (collectively, Software), is granted only
+ * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
+ * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
+ * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
+ * For further information, contact community.ruggedboard.com / info@ruggedboard.com
+ *
+ * Application: 
+ * Brief: 
+ * Author: srikrishna KN ( kn.kingkrishna@gmail.com )
+ * Title: Project engineer
+ * Last Modified Date: 23.03.2023
+ *
+/*********************************************************************************************/
+
+// Header File
+#include <stdio.h>
+
+// Main Function
+int main()
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(int));
+    printf("Size of short: %lu byte(s)\n", sizeof(unsigned int));
+    //printf("Size of int: %lu byte(s)\n", sizeof(int));
+    printf("Size of long: %lu byte(s)\n", sizeof(long));
+    printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+    printf("Size of float: %lu byte(s)\n", sizeof(float));
+    printf("Size of double: %lu byte(s)\n", sizeof(double));
+    printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+     
+     return 0;
+}
+
+// Program End
